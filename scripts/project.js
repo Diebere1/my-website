1b5b78ee4d1502456b0a76e8eac7f142800b6e8c
Emmanuel <ethelbertemmanuel1@gmail.com>
2024-05-07 23:05:37 +0100

    my-web-application

---
+++ scripts/project.js
@@ -0,0 +1,8 @@
+// case study buttons
+const caseStudyBtn = document.querySelectorAll('.case-btn');
+
+caseStudyBtn.forEach(caseBtn => {
+  caseBtn.addEventListener('click', () => {
+    alert('Something went wrong!')
+  })
+})
